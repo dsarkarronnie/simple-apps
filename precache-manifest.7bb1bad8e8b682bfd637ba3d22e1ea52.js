384a5286f9ff7e284f805de0ac0ffae7087b15af
dsarkarronnie <dsarkar.ronnie@gmail.com>
2020-08-27 01:30:08 +0530

    Updates

---
+++ precache-manifest.7bb1bad8e8b682bfd637ba3d22e1ea52.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f815242d707004cb8fa8cab5b4d00d71",
+    "url": "/simple-apps/index.html"
+  },
+  {
+    "revision": "efbd21faeda7e6f3ec9f",
+    "url": "/simple-apps/static/css/2.611e3826.chunk.css"
+  },
+  {
+    "revision": "a58192d9a7372fc27974",
+    "url": "/simple-apps/static/css/main.57078e49.chunk.css"
+  },
+  {
+    "revision": "efbd21faeda7e6f3ec9f",
+    "url": "/simple-apps/static/js/2.305d86c5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/simple-apps/static/js/2.305d86c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be882ae76281f04daaa0",
+    "url": "/simple-apps/static/js/3.d064a2f5.chunk.js"
+  },
+  {
+    "revision": "92cc4ac4f2d725d0a311",
+    "url": "/simple-apps/static/js/4.e4240afb.chunk.js"
+  },
+  {
+    "revision": "a58192d9a7372fc27974",
+    "url": "/simple-apps/static/js/main.2625089d.chunk.js"
+  },
+  {
+    "revision": "a31f45ac6a2bbf82ce3a",
+    "url": "/simple-apps/static/js/runtime-main.c21eec23.js"
+  },
+  {
+    "revision": "c7a33805ffda0d32bd2a9904c8b02750",
+    "url": "/simple-apps/static/media/color.c7a33805.png"
+  },
+  {
+    "revision": "567f57385ea3dde2c9aec797d07850d2",
+    "url": "/simple-apps/static/media/line.567f5738.gif"
+  },
+  {
+    "revision": "177cc92d2e8027712a8c1724abd272cd",
+    "url": "/simple-apps/static/media/open-sans-v15-latin-300.177cc92d.ttf"
+  },
+  {
+    "revision": "27ef0b062b2e221df16f3bbd97c2dca8",
+    "url": "/simple-apps/static/media/open-sans-v15-latin-300.27ef0b06.svg"
+  },
+  {
+    "revision": "521d17bc9f3526c690e8ada6eee55bec",
+    "url": "/simple-apps/static/media/open-sans-v15-latin-300.521d17bc.woff"
+  },
+  {
+    "revision": "60c866748ff15f5b347fdba64596b1b1",
+    "url": "/simple-apps/static/media/open-sans-v15-latin-300.60c86674.woff2"
+  },
+  {
+    "revision": "76b56857ebbae3a5a689f213feb11af0",
+    "url": "/simple-apps/static/media/open-sans-v15-latin-300.76b56857.eot"
+  },
+  {
+    "revision": "148a6749baa5f658a45183ddb5ee159f",
+    "url": "/simple-apps/static/media/open-sans-v15-latin-700.148a6749.eot"
+  },
+  {
+    "revision": "2e00b2635b51ba336b4b67a5d0bc03c7",
+    "url": "/simple-apps/static/media/open-sans-v15-latin-700.2e00b263.svg"
+  },
+  {
+    "revision": "623e3205570002af47fc2b88f9335d19",
+    "url": "/simple-apps/static/media/open-sans-v15-latin-700.623e3205.woff"
+  },
+  {
+    "revision": "7e08cc656863d52bcb5cd34805ac605b",
+    "url": "/simple-apps/static/media/open-sans-v15-latin-700.7e08cc65.ttf"
+  },
+  {
+    "revision": "d08c09f2f169f4a6edbcf8b8d1636cb4",
+    "url": "/simple-apps/static/media/open-sans-v15-latin-700.d08c09f2.woff2"
+  },
+  {
+    "revision": "7aab4c13671282c90669eb6a10357e41",
+    "url": "/simple-apps/static/media/open-sans-v15-latin-regular.7aab4c13.svg"
+  },
+  {
+    "revision": "9dce7f01715340861bdb57318e2f3fdc",
+    "url": "/simple-apps/static/media/open-sans-v15-latin-regular.9dce7f01.eot"
+  },
+  {
+    "revision": "bf2d0783515b7d75c35bde69e01b3135",
+    "url": "/simple-apps/static/media/open-sans-v15-latin-regular.bf2d0783.woff"
+  },
+  {
+    "revision": "c045b73d86803686f4cd1cc3f9ceba59",
+    "url": "/simple-apps/static/media/open-sans-v15-latin-regular.c045b73d.ttf"
+  },
+  {
+    "revision": "cffb686d7d2f4682df8342bd4d276e09",
+    "url": "/simple-apps/static/media/open-sans-v15-latin-regular.cffb686d.woff2"
+  },
+  {
+    "revision": "159263bf4976843830365d52d1f94fba",
+    "url": "/simple-apps/static/media/primeicons.159263bf.eot"
+  },
+  {
+    "revision": "2e3a6b48408add8bb604d0b4e820a3b5",
+    "url": "/simple-apps/static/media/primeicons.2e3a6b48.ttf"
+  },
+  {
+    "revision": "4e915114c908962a6ff2d505c2a4816b",
+    "url": "/simple-apps/static/media/primeicons.4e915114.svg"
+  },
+  {
+    "revision": "79432094e4838b36d974b9d8c57d22e2",
+    "url": "/simple-apps/static/media/primeicons.79432094.woff"
+  }
+]);
